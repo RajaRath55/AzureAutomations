e5a9ac6777d3252f29bb8ef51ba3e1ea212783d7
RajaRath55 <72096878+RajaRath55@users.noreply.github.com>
2023-03-03 18:04:43 +0530

    Delete DataRead.py

--- ADB/DataRead.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-diamonds_df = (spark.read
-  .format("csv")
-  .option("mode", "PERMISSIVE")
-  .load("/Workspace/Repos/rajatrath872@gmail.com/AzureAutomations/ADB/annual-enterprise-survey-2021-financial-year-provisional-csv.csv")
-)
